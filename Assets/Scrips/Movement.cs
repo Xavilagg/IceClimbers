917acf6a7afa16c6331fccafe975e4bc901c8060
Xavilagg <xavier.castro.adame@gmail.com>
2025-04-10 16:14:36 -0600

    Clase 1.1

--- Assets/Scrips/Movement.cs
+++ Assets/Scrips/Movement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    public float movespeed = 0;
+    Vector3 direction = Vector3.zero;
+    private void Start ()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.LinearVelocityX = direction.x * movespeed;
+    }
+    public void Move_Event (InputAction.CallbackContex context)
+    {
+        if(context.performed)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+            direction.x = input.x;
+            direction.z = input.y;
+        }
+
+        else
+        {
+            direction = Vector3.zero;
+        }
     }
 }
